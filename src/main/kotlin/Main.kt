e21a3e404957f12c457812f2fc97f2b4a39b1abf
DrDisagree <crazymahmud08@gmail.com>
2024-08-12 20:15:34 +0600

    Add logs management

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,16 +8,14 @@ import dev.inmo.tgbotapi.extensions.utils.asCommonUser
 import dev.inmo.tgbotapi.extensions.utils.asPrivateChat
 import dev.inmo.tgbotapi.extensions.utils.extensions.raw.from
 import dev.inmo.tgbotapi.types.ChatIdentifier
+import dev.inmo.tgbotapi.types.IdChatIdentifier
 import dev.inmo.tgbotapi.types.UserId
 import dev.inmo.tgbotapi.utils.PreviewFeature
 import dev.inmo.tgbotapi.utils.RiskFeature
 import i18n.getModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-
-/**
- * This is one of the most easiest bot - it will just print information about itself
- */
+import java.util.Locale
 
 data class JoinRequest(
     val identifier: ChatIdentifier,
@@ -26,15 +24,45 @@ data class JoinRequest(
 
 @OptIn(PreviewFeature::class, RiskFeature::class)
 suspend fun main(vararg args: String) {
-    val map = MaxSizeHashMap<ChatIdentifier, JoinRequest>(128)
-    if (args.size != 1) {
-        println("Usage: java -jar joinreqbot.jar {BotToken}")
+    var botToken: String? = null
+    var acceptedKeyword: String? = null
+    var logGroupId: IdChatIdentifier? = null
+
+    if (args.isEmpty() || args.size % 2 != 0 || args.size < 4) {
+        printBotUsage()
         return
     }
 
-    println("BotToken: ${args[0]}")
+    for (i in args.indices) {
+        when (args[i]) {
+            "--token" -> {
+                if (i + 1 < args.size) {
+                    botToken = args[i + 1]
+                }
+            }
 
-    telegramBotWithBehaviourAndLongPolling(args[0], CoroutineScope(Dispatchers.IO)) {
+            "--keyword" -> {
+                if (i + 1 < args.size) {
+                    acceptedKeyword = args[i + 1]
+                }
+            }
+
+            "--log" -> {
+                if (i + 1 < args.size) {
+                    logGroupId = IdChatIdentifier(args[i + 1].toLong())
+                }
+            }
+        }
+    }
+
+    if (botToken == null || acceptedKeyword == null) {
+        printBotUsage()
+        return
+    }
+
+    val map = MaxSizeHashMap<ChatIdentifier, JoinRequest>(128)
+
+    telegramBotWithBehaviourAndLongPolling(botToken, CoroutineScope(Dispatchers.IO)) {
         onChatJoinRequest {
             val userId = it.from.id
             val chatId = it.chat.id
@@ -44,14 +72,19 @@ suspend fun main(vararg args: String) {
             bot.sendMessage(userId, model.question)
             map[userId] = JoinRequest(chatId, userId)
 
-            println("user $userId start joining $chatId, map size: $mapSize")
+            println("User $userId started joining $chatId, map size: $mapSize")
+        }
+
+        onCommandWithArgs("help") { it, _ ->
+            bot.sendMessage(
+                it.chat,
+                getModel(it.from?.asCommonUser()?.ietfLanguageCode?.code).usage
+            )
         }
 
         onCommandWithArgs("join") { it, args ->
             val user = it.chat.asPrivateChat()!!
             val model = getModel(it.from?.asCommonUser()?.ietfLanguageCode?.code)
-
-            println(it)
 
             if (args.size <= 1) {
                 bot.sendMessage(user.id, model.usage)
@@ -61,25 +94,49 @@ suspend fun main(vararg args: String) {
             if (map.containsKey(user.id)) {
                 val req = map[user.id]!!
                 val message = it.content.text
+                val userName = user.username ?: "User"
+                val mention = "[$userName](tg://user?id=${user.id.chatId})"
+                val logMessage: String
 
-                if (message.contains("pixel", ignoreCase = true)) {
+                if (message.contains(
+                        acceptedKeyword.lowercase(Locale.getDefault()),
+                        ignoreCase = true
+                    )
+                ) {
                     bot.approveChatJoinRequest(req.identifier, user.id)
                     bot.sendMessage(it.chat, model.correct)
 
-                    println("user ${user.id} joined ${req.identifier}")
+                    logMessage = "#APPROVE:\n" +
+                            "Chat: ${req.identifier}\n" +
+                            "User: $mention\n" +
+                            "User ID: ${user.id.chatId}\n" +
+                            "Response: $message"
+                    println("User ${user.id} joined ${req.identifier}")
 
                     map.remove(user.id)
                 } else {
                     bot.declineChatJoinRequest(req.identifier, user.id)
                     bot.sendMessage(user.id, model.incorrect)
 
-                    println("user ${user.id} join ${req.identifier} failed")
+                    logMessage = "#REJECT:\n" +
+                            "Chat: ${req.identifier}\n" +
+                            "User: $mention\n" +
+                            "User ID: ${user.id.chatId}\n" +
+                            "Response: $message"
+                    println("User ${user.id} failed to join ${req.identifier}")
+                }
+
+                logGroupId?.let { groupId ->
+                    bot.sendMessage(groupId, logMessage)
                 }
             } else {
                 bot.sendMessage(user.id, model.notFound)
-
-                println("user ${user.id} not found in group")
+                println("User ${user.id} not found in group")
             }
         }
     }.second.join()
+}
+
+private fun printBotUsage() {
+    println("Usage: java -jar joinreqbot.jar --token {BotToken} --keyword {AcceptedKeyword} --log {LogGroupId}")
 }
